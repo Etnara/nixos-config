94458fde0e33f0794eba58c401f82806aee6fd9f
ren <zakbutt18@gmail.com>
2025-10-06 18:04:12 -0400

    Make mpv loop on short videos/gifs

--- home-manager/applications/mpv.nix
+++ home-manager/applications/mpv.nix
@@ -4,6 +4,13 @@
 
   programs.mpv = {
     enable = true;
+    profiles = {
+      short = {
+        profile-cond = "duration < 10";
+        loop-file = "inf";
+        profile-restore = "copy";
+      };
+    };
   };
 
 }
