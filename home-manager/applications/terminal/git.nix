30f3b42ffec01bda4581f99e824c2098001f9b34
ren <zakbutt18@gmail.com>
2025-10-06 19:06:21 -0400

    Install Delta, Bat, and set pull.rebase=true for Git

--- home-manager/applications/terminal/git.nix
+++ home-manager/applications/terminal/git.nix
@@ -4,15 +4,19 @@
 
   programs.git = {
     enable = true;
+    delta.enable = true;
     userName = userSettings.username;
     userEmail = userSettings.email;
     extraConfig = {
       init.defaultBranch = "master";
+      pull.rebase = "true";
       core.editor = "nvim";
       diff.tool = "vimdiff";
       merge.tool = "vimdiff";
     };
   };
+
+  programs.bat.enable = true;
 
 }
 
--- home-manager/theme/catppuccin.nix
+++ home-manager/theme/catppuccin.nix
@@ -6,10 +6,12 @@
     enable = false;
     flavor = "mocha";
     accent = "mauve";
+    bat.enable = true;
     brave.enable = true; # TODO: Doesn't work
     btop.enable = true;
     cursors.enable = true;
     cursors.accent = "dark";
+    delta.enable = true;
     dunst.enable = true;
     fcitx5.enable = true;
     fcitx5.enableRounded = true;
