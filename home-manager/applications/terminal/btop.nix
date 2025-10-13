693a5c566e1681f287e437a839a50dc1ccb9f721
ren <zakbutt18@gmail.com>
2025-10-13 12:04:06 -0400

    Add GPU support to Btop

--- home-manager/applications/terminal/btop.nix
+++ home-manager/applications/terminal/btop.nix
@@ -1,9 +1,13 @@
-{ ... }:
+{ pkgs, systemSettings, ... }:
 
 {
   
   programs.btop= {
     enable = true;
+    package = pkgs.btop.override {
+      cudaSupport = systemSettings.gpuType == "nvidia";
+      rocmSupport = systemSettings.gpuType == "amd";
+    };
     settings = {
       # color_theme = "catppuccin_mocha";
       vim_keys = true;
