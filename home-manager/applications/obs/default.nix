786b5b8a394db50461b2f4971cef866e02c5c7b5
ren <zakbutt18@gmail.com>
2025-10-14 22:59:01 -0400

    Pin OBS to version 31.1.2
    Version 32.0.0 removed the --disable-shutdown-check flag without fixing
    the erroneous "unsafe shutdown" warning every time you boot.

--- home-manager/applications/obs/default.nix
+++ home-manager/applications/obs/default.nix
@@ -1,8 +1,17 @@
-{ pkgs, config, userSettings, lib, ... }:
+{ pkgs, config, userSettings, lib, systemSettings, ... }:
 
+let
+  pkgs-obs-31 = import (pkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "e473c61292725842b565ca037aa5c0ddf808994e";
+    sha256 = "sha256-kqV6U5a4zv4+S05tkl/hQQ1FYLDJtqOjUMB4u3Tr8l8=";
+  }) { inherit (pkgs) system; };
+in
 {
 
   programs.obs-studio.enable = true;
+  programs.obs-studio.package = pkgs-obs-31.obs-studio;
   programs.obs-studio.plugins = with pkgs.obs-studio-plugins; [
     obs-pipewire-audio-capture
   ];
